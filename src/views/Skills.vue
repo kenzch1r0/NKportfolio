9b9e5389f0cdfde58c159d243b79e50c86c7600b
kench1r0 <nattawut.eleven@gmail.com>
2024-07-14 17:33:29 +0700

    update title and about me

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -2,17 +2,17 @@
   <section class="content">
 			<div class="" data-aos="fade-right">
         <h2>ABOUT ME</h2>
+        <br/>
       <div>
-        <p> Descripe me with Top 5 CliftonStrengths </p>
-        <v-row>
-          <b-col>
+        <div> 
+          <h5>Descripe me with Top 5 CliftonStrengths</h5>
             <img src="https://img.shields.io/badge/-Learner-01945D?style=flat-square" style="border-radius: 4px;" v-b-tooltip.hover title="People exceptionally talented in the Learner theme have a great desire to learn and want to continuously improve. The process of learning, rather than the outcome, excites them."><p class="pipe">|</p>
             <img src="https://img.shields.io/badge/Individualization-016FCC?style=flat-square" style="border-radius: 4px;" v-b-tooltip.hover title="People exceptionally talented in the Individualization theme are intrigued with the unique qualities of each person. They have a gift for figuring out how different people can work together productively."><p class="pipe">|</p>
             <img src="https://img.shields.io/badge/Relator-016FCC?style=flat-square" style="border-radius: 4px;" v-b-tooltip.hover title="People exceptionally talented in the Relator theme enjoy close relationships with others. They find deep satisfaction in working hard with friends to achieve a goal."><p class="pipe">|</p>
             <img src="https://img.shields.io/badge/Achiever-7A2280?style=flat-square" style="border-radius: 4px;" v-b-tooltip.hover title="People exceptionally talented in the Achiever theme work hard and possess a great deal of stamina. They take immense satisfaction in being busy and productive."><p class="pipe">|</p>
             <img src="https://img.shields.io/badge/Intellection-01945D?style=flat-square" style="border-radius: 4px;" v-b-tooltip.hover title="People exceptionally talented in the Intellection theme are characterized by their intellectual activity. They are introspective and appreciate intellectual discussions.">
-          </b-col>
-        </v-row>
+        </div>
+      <br/>
       </div>
           <skill  title="SKILLS" :data="skills" ref="Skill"></skill>
       </div>
